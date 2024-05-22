e4d9a3a91dae5af8cec6c2858dc678d31f1da92e
Anastasia-Filipenko <filipenko.nastyha@gmail.com>
2024-05-22 15:09:12 +0300

    fix

--- schemas/userSchemas.js
+++ schemas/userSchemas.js
@@ -3,7 +3,6 @@ import Joi from "joi";
 export const userRegisterSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
-  subscription: Joi.string().valid("starter", "pro", "business").required(),
 });
 
 export const userLoginSchema = Joi.object({
